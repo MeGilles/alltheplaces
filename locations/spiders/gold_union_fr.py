7ff8fc5937fc551f06f787d3bf3ce8a8231c8450
Your Name <35843078+MeGilles@users.noreply.github.com>
2026-08-26 09:35:15 +0200

    add opening hours

--- locations/spiders/gold_union_fr.py
+++ locations/spiders/gold_union_fr.py
@@ -3,14 +3,25 @@ from typing import Iterable
 from locations.categories import Categories, apply_category
 from locations.items import Feature
 from locations.storefinders.stockist import StockistSpider
+from locations.hours import OpeningHours, DELIMITERS_FR, DAYS_FR
+import re
 
 
 class GoldUnionFRSpider(StockistSpider):
-    name = "gold_union_fr"
-    item_attributes = {"brand": "Gold Union", "brand_wikidata": "Q131622916"}
-    key = "u20465"
+    name : str = "gold_union_fr"
+    item_attributes : set = {"brand": "Gold Union", "brand_wikidata": "Q131622916"}
+    key : str = "u20465"
 
     def parse_item(self, item: Feature, location: dict) -> Iterable[Feature]:
         item["branch"] = item.pop("name")
+        
+        item["opening_hours"] = OpeningHours()
+        for custom_fields in (location.get("custom_fields") or []):
+            opening = (custom_fields.get("name") or "").strip().lower()
+            if opening == "ouverture":
+                opening_hours = (custom_fields.get("value") or "").strip()                
+                opening_hours = re.sub(r"(\d{1,2})h(\d{2})", r"\1:\2", opening_hours)
+                item["opening_hours"].add_ranges_from_string(opening_hours, DAYS_FR, delimiters=DELIMITERS_FR)
+
         apply_category(Categories.SHOP_GOLD_BUYER, item)
         yield item
